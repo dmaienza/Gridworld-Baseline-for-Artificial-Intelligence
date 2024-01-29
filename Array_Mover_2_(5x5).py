a22bd31188b7d3c57e860b54749b2ac65b5c1da9
Daniel Maienza <146412743+dmaienza@users.noreply.github.com>
2024-01-29 13:18:00 -0500

    Added main programs

---
+++ Array_Mover_2_(5x5).py
@@ -0,0 +1,170 @@
+import numpy as np
+from version6_2 import animate
+# import matplotlib.pyplot as plt
+
+start = [0, 0]
+goal = [4, 4]
+
+# possible actions
+actionUp = 0
+actionDown = 1
+actionLeft = 2
+actionRight = 3
+actions = [actionUp, actionDown, actionLeft, actionRight]
+world_height = 5
+world_width = 5
+c = np.array([[0, 0, 1, 0, 0],
+              [0, 0, 1, 0, 0],
+              [0, 0, 0, 0, 0],
+              [0, 0, 1, 0, 0],
+              [0, 0, 1, 0, 0]])
+num_iter = 10
+# R1 = np.zeros(num_iter - 1, dtype=int)
+# R2 = np.zeros(num_iter - 1, dtype=int)
+# d = np.arange(0, num_iter - 1, 1)
+num_states = world_height * world_width     # 5*5=25
+idx = range(num_states)
+V = np.zeros((world_height, world_width))
+V_old = V.copy()
+gamma = 1
+
+
+def move(state, action):
+
+    i, j = state
+
+    if action == actionUp:
+        state_n = [max(i - 1, 0), j]
+    elif action == actionDown:
+        state_n = [min(i + 1, world_height - 1), j]
+    elif action == actionLeft:
+        state_n = [i, max(j - 1, 0)]
+    elif action == actionRight:
+        state_n = [i, min(j + 1, world_width - 1)]
+    else:
+        assert False
+
+    # check for obstacles
+    if c[state_n[0], state_n[1]] == 1:
+        return state
+    else:
+        return state_n
+
+
+def reward(state):
+    if state == goal:
+        return 10
+    else:
+        return -1
+
+
+def random_agent(start_state):
+    # R = 0
+    T = np.zeros((2, 1))
+    A = []
+    state = start_state
+    i = 0
+    T[:, i] = state
+    while state != goal:
+        action_random = np.random.randint(0, 4)
+        A.append(action_random)
+        state = move(state, action_random)
+        # R1[d[i + 1]] = reward(state) + R1[d[i]]
+        # print(R1)
+        T = np.append(arr=T, values=np.zeros((2, 1)), axis=1)
+        # print(T)
+        i += 1
+        T[:, i] = state
+    return T, A # , R
+
+
+def coord_to_idx(row, col):
+    return row * world_width + col
+
+
+def idx_to_coord(idx):
+    col = idx % world_width
+    row = idx // world_width
+    state = [row, col]
+    return state
+
+
+# Value Function
+print("Value Function")
+for t in range(num_iter):
+    for idx in range(num_states):
+        state = idx_to_coord(idx)
+        sr, sc = state
+        tmp = []
+        if state != goal:
+            for a in actions:
+                sp = move(state, a)
+                # R2[t] += reward(sp)
+                tmp.append(reward(sp) + gamma * V_old[sp[0], sp[1]])
+            V[sr, sc] = max(tmp)
+        else:
+            V[sr, sc] = 0
+    V_old = V
+    print(V)
+    print("_______________________")
+
+# Policy
+P = np.zeros((world_height, world_width))
+for idx in range(num_states):
+    state = idx_to_coord(idx)
+    sr, sc = state
+    tmp = []
+    for a in actions:
+        sp = move(state, a)
+        tmp.append(reward(sp) + gamma * V[sp[0], sp[1]])
+    idx_best_action = np.argmax(tmp)
+    best_action = actions[idx_best_action]
+    P[sr, sc] = best_action
+
+print("Policy")
+print(P)
+print("_______________________")
+
+
+def path(start_state):
+    # R2 = 0
+    T = np.zeros((2, 1))
+    A = []
+    # print(T)
+    state = start_state
+    i = 0
+    T[:, i] = state
+    while state != goal:
+        a, b = state
+        action_policy = P[a, b]
+        A.append(action_policy)
+        state = move(state, action_policy)
+        # R2 += reward(state)
+        # R2[d[i]] = reward(state) + R2[d[i-1]]
+        # print(R2)
+        T = np.append(arr=T, values=np.zeros((2, 1)), axis=1)
+        i += 1
+        T[:, i] = state
+        # print(T)
+    return T, A # , R2
+
+
+trajectory, action = path(start)
+# trajectory, action = random_agent(start)
+animate(trajectory, goal, action)
+
+# R3 = np.zeros(20)
+# for i in range(20):
+#     trajectory, R2 = path(start)
+#     R3[i] = R2
+
+# plot reward value vs steps
+# plt.plot(d, R1, 'o-', color='blue', label="Randomized")
+# plt.plot(R2, 'o-', color='red', label="Learning Algorithm")
+# plt.title('Sum of Reward During Episode Vs Number of Episodes')
+# plt.legend()
+# plt.xlabel('Number of Episodes')
+# plt.ylabel('Sum of Reward During Episode')
+# plt.axvline(x=0, color='red')
+# plt.axhline(y=0, color='red')
+# plt.show()
---
+++ version6_3.py
@@ -0,0 +1,166 @@
+import pygame
+# import numpy as np
+
+# Variables
+size = 1000
+rows = 7
+t = 100
+border = size / (2 + rows)
+border_rounded = size // (2 + rows)
+margin = border_rounded // 10
+
+bg_color = pygame.Color("black")
+color_line = pygame.Color("grey")
+border_color = pygame.Color("grey")
+player_color = pygame.Color("red")
+rewardColor = pygame.Color("green")
+
+actionUp = 0
+actionDown = 1
+actionLeft = 2
+actionRight = 3
+
+pygame.init()
+
+# Draw the scenario
+screen = pygame.display.set_mode((size, size))
+
+# Filling the background
+screen.fill(bg_color)
+
+# Set title and key icon
+pygame.display.set_caption("Gym-minigrid")
+key_icon = pygame.image.load('key.png')
+pygame.display.set_icon(key_icon)
+
+
+def draw_base(goal):
+    # Drawing the border
+    pygame.draw.rect(screen, border_color, pygame.Rect((0, 0), (size, border)))
+    pygame.draw.rect(screen, border_color, pygame.Rect((0, 0), (border, size)))
+    pygame.draw.rect(screen, border_color, pygame.Rect((0, size - border_rounded), (size, border)))
+    pygame.draw.rect(screen, border_color, pygame.Rect((size - border_rounded, 0), (border, size)))
+
+    # draw the obstacles
+    pygame.draw.rect(screen, border_color, pygame.Rect((border, 2 * border), (4 * border + 1, border + 1)))
+    pygame.draw.rect(screen, border_color, pygame.Rect((6 * border, 2 * border), (2 * border + 1, border + 1)))
+    pygame.draw.rect(screen, border_color, pygame.Rect((2 * border, 4 * border), (6 * border + 1, border + 1)))
+    pygame.draw.rect(screen, border_color, pygame.Rect((5 * border, 5 * border), (border, border)))
+    pygame.draw.rect(screen, border_color, pygame.Rect((5 * border, 7 * border), (border, border + 1)))
+    pygame.draw.rect(screen, rewardColor,
+                     pygame.Rect((goal[0] * border + border, goal[1] * border + border), (border, border)))
+
+
+def grid():
+    # Formulate grid by drawing separated lines
+    distance_btw_rows = border
+    x = 0
+    y = 0
+    for i in range(rows - 1):
+        # increment x and y to separate lines
+        x += distance_btw_rows
+        y += distance_btw_rows
+        # vertical lines
+        pygame.draw.line(screen, color_line, (x + border, border),
+                         (x + border, size - border))
+        # horizontal lines
+        pygame.draw.line(screen, color_line, (border, y + border),
+                         (size - border, y + border))
+
+
+# Define the classes
+class Player:
+
+    def __init__(self, initial_state, action):
+        self.x1 = 0
+        self.y1 = 0
+        self.x2 = 0
+        self.y2 = 0
+        self.x3 = 0
+        self.y3 = 0
+        self.state2coordinate(initial_state, action)
+
+    def show(self, color):
+        global screen
+        pygame.draw.polygon(screen, color, ((self.x1, self.y1), (self.x2, self.y2), (self.x3, self.y3)))
+
+    def state2coordinate(self, state, action):
+        horizontal = state[1] * border + border + margin
+        vertical = state[0] * border + border + margin
+        side = border - 2 * margin
+
+        if action == actionLeft:
+            self.x1 = horizontal + side
+            self.y1 = vertical + side
+            self.x2 = horizontal
+            self.y2 = vertical + side / 2
+            self.x3 = horizontal + side
+            self.y3 = vertical
+        if action == actionRight:
+            self.x1 = horizontal
+            self.y1 = vertical
+            self.x2 = horizontal + side
+            self.y2 = vertical + side / 2
+            self.x3 = horizontal
+            self.y3 = vertical + side
+        if action == actionUp:
+            self.x1 = horizontal
+            self.y1 = vertical + side
+            self.x2 = horizontal + side / 2
+            self.y2 = vertical
+            self.x3 = horizontal + side
+            self.y3 = vertical + side
+        if action == actionDown:
+            self.x1 = horizontal + side
+            self.y1 = vertical
+            self.x2 = horizontal + side / 2
+            self.y2 = vertical + side
+            self.x3 = horizontal
+            self.y3 = vertical
+
+    def update(self, state, action):
+        self.show(bg_color)
+        self.state2coordinate(state, action)
+        self.show(player_color)
+
+
+# update display
+def redraw(goal, player, state, action):
+    draw_base(goal)
+    grid()
+    player.update(state, action)
+    pygame.display.flip()
+    pygame.time.wait(t)
+
+
+def animate(T, goal, action):
+    # Create Objects
+    player = Player(T[:, 0], action[0])  # initial state
+    player.show(player_color)
+    pygame.time.wait(3000)
+    for i in range(T.shape[1]):
+        state = T[:, i]
+        print(state)
+        redraw(goal, player, state, action[i - 1])
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                print("quit")
+                pygame.display.quit()
+                pygame.quit()
+                exit()
+
+    running = True
+    while running:
+        # redraw the surface and update it repeatedly
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                print("quit")
+                pygame.display.quit()
+                pygame.quit()
+                exit()
+
+
+# if __name__ == "__main__":
+#     T = np.array([[0, 1, 2, 2, 2, 2, 3, 4, 4], [0, 0, 0, 1, 2, 3, 3, 3, 4]])
+#     animate(T, goal)
